9be2915d2c63d61bb606736faf349a290d275277
btrefethen01-alt <btrefethen01@gmail.com>
2026-03-13 16:42:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,5 +53,6 @@ if ingredients_list:
     
         st.success('Your Smoothie is ordered!', icon="✅")
 
+import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
